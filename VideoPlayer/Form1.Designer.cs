e2822e5235538077a278b95c20d93b7d9da574f2
Ken Wang <ykenwang@gmail.com>
2016-04-16 20:20:24 -0400

    Initial upload
    This is a class example for CIST 0150.

---
+++ VideoPlayer/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace VideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.fsButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.volumnTrackBar = new System.Windows.Forms.TrackBar();
+            this.progressTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volumnTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(669, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(717, 49);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(717, 168);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(717, 139);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 2;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(28, 437);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(764, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // fsButton
+            // 
+            this.fsButton.Location = new System.Drawing.Point(717, 78);
+            this.fsButton.Name = "fsButton";
+            this.fsButton.Size = new System.Drawing.Size(75, 23);
+            this.fsButton.TabIndex = 1;
+            this.fsButton.Text = "Fullscreen";
+            this.fsButton.UseVisualStyleBackColor = true;
+            this.fsButton.Click += new System.EventHandler(this.fsButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.progressLabel.Location = new System.Drawing.Point(692, 412);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(100, 23);
+            this.progressLabel.TabIndex = 5;
+            this.progressLabel.Text = "0";
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // volumnTrackBar
+            // 
+            this.volumnTrackBar.Location = new System.Drawing.Point(717, 223);
+            this.volumnTrackBar.Name = "volumnTrackBar";
+            this.volumnTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.volumnTrackBar.Size = new System.Drawing.Size(45, 162);
+            this.volumnTrackBar.TabIndex = 4;
+            this.volumnTrackBar.Scroll += new System.EventHandler(this.volumnTrackBar_Scroll);
+            // 
+            // progressTrackBar
+            // 
+            this.progressTrackBar.Location = new System.Drawing.Point(28, 482);
+            this.progressTrackBar.Name = "progressTrackBar";
+            this.progressTrackBar.Size = new System.Drawing.Size(764, 45);
+            this.progressTrackBar.TabIndex = 7;
+            this.progressTrackBar.Scroll += new System.EventHandler(this.progressTrackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 550);
+            this.Controls.Add(this.progressTrackBar);
+            this.Controls.Add(this.volumnTrackBar);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.fsButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Video Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.volumnTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button fsButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.TrackBar volumnTrackBar;
+        private System.Windows.Forms.TrackBar progressTrackBar;
+    }
+}
+
---
+++ VideoPlayer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MyVideo;
+using System.IO;
+
+namespace VideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        Media newMedia = new Media();
+        OpenFileDialog file;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            file = new OpenFileDialog();
+
+            file.Filter = "Video Files | *.avi; *.mpg; *.wmv; *.mkv; *.mpeg; *.mp4; *.rm; *.wav; *.mp3";
+            file.InitialDirectory = Directory.GetCurrentDirectory();
+            file.ShowDialog();
+
+            if (file.FileName != "")
+            {
+                newMedia.Open(file.FileName, pictureBox1);
+                playButton.Enabled = true;
+                stopButton.Enabled = true;
+                pauseButton.Enabled = true;
+                fsButton.Enabled = true;
+                volumnTrackBar.Value = 5;
+                progressBar1.Maximum = newMedia.Duration();
+                progressTrackBar.Maximum = newMedia.Duration();
+                newMedia.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playButton.Enabled = false;
+            stopButton.Enabled = false;
+            pauseButton.Enabled = false;
+            fsButton.Enabled = false;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            newMedia.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void fsButton_Click(object sender, EventArgs e)
+        {
+            newMedia.FullScreen();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            newMedia.Close();
+            newMedia.Open(file.FileName, pictureBox1);
+            newMedia.Play();
+            timer1.Enabled = true;
+            newMedia.Volume = volumnTrackBar.Value*100;
+            newMedia.Position = progressTrackBar.Value;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            newMedia.Pause = !newMedia.Pause;
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = newMedia.Position;
+            progressTrackBar.Value = newMedia.Position;
+
+          //  progressLabel.Text = newMedia.Position.ToString();
+            progressLabel.Text = (newMedia.Position / Convert.ToDecimal(newMedia.Duration())).ToString("P0");
+        }
+
+        private void volumnTrackBar_Scroll(object sender, EventArgs e)
+        {
+            newMedia.Volume = volumnTrackBar.Value * 100;
+        }
+
+        private void progressTrackBar_Scroll(object sender, EventArgs e)
+        {
+            newMedia.Position = progressTrackBar.Value;
+        }
+
+    }
+}
